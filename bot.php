477f26ebb7e9394bf7107d4faf5cc62b73c9637d
Olexandr Panov <futupas@gmail.com>
2018-09-16 00:44:15 +0300

    strtolower_my

--- bot.php
+++ bot.php
@@ -1,9 +1,19 @@
 <?php
-    setlocale(LC_CTYPE, 'ru_RU');
-    file_put_contents('logs.txt', json_encode(system('locale -a')), FILE_APPEND);
     // 636469447:AAEnwoa5s_Ati_miiHQukaIhSalotJSk0Ts
     // api.telegram.org/bot636469447:AAEnwoa5s_Ati_miiHQukaIhSalotJSk0Ts
-
+    
+    function strtolower_my($str) {
+        $str = strtolower($str);
+        $search = array(
+        'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Ь','Б','Ю','Ё','І','Ї'
+        );
+        $replace = array(
+        'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','ь','б','ю','ё','і','ї'
+        );
+        $str = str_replace($search, $replace, $str);
+        return $str;
+    };
+        
     function SendMessage($chatid, $text) {
         $response = file_get_contents('https://api.telegram.org/bot636469447:AAEnwoa5s_Ati_miiHQukaIhSalotJSk0Ts/sendMessage?chat_id='.$chatid.'&text=you'.$text);
     };
@@ -42,7 +52,7 @@
         $readymsg = str_replace("-", "", $readymsg);
         $readymsg = str_replace(")", "", $readymsg);
         $readymsg = str_replace("(", "", $readymsg);
-        $words = explode(" ", strtolower($readymsg));
+        $words = explode(" ", strtolower_my($readymsg));
 
         file_put_contents('logs.txt', $msg."\n", FILE_APPEND);
         file_put_contents('logs.txt', $readymsg."\n", FILE_APPEND);
