b00c9992ee65bd6533910aa3c767cb58e0bdfc02
Olexandr Panov <futupas@gmail.com>
2018-09-16 12:23:19 +0300

    not str but str2

--- bot.php
+++ bot.php
@@ -3,15 +3,15 @@
     // api.telegram.org/bot636469447:AAEnwoa5s_Ati_miiHQukaIhSalotJSk0Ts
     
     function strtolower_my($str) {
-        $str = strtolower($str);
+        $str2 = strtolower($str);
         $search = array(
         'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Ь','Б','Ю','Ё','І','Ї'
         );
         $replace = array(
         'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','ь','б','ю','ё','і','ї'
         );
-        $str = str_replace($search, $replace, $str);
-        return $str;
+        $str2 = str_replace($search, $replace, $str2);
+        return $str2;
     };
         
     function SendMessage($chatid, $text) {
