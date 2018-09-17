a6f7fee049cdecd7cdb08f0602ea68d53023cc03
Olexandr Panov <futupas@gmail.com>
2018-09-18 00:04:37 +0300

    timer ready

--- timer.php
+++ timer.php
@@ -1,14 +1,25 @@
 <?php
     // https://johndowntgbot.herokuapp.com/timer.php
 
+    function SendMessage($chatid, $text) {
+        $response = file_get_contents('https://api.telegram.org/bot636469447:AAEnwoa5s_Ati_miiHQukaIhSalotJSk0Ts/sendMessage?chat_id='.$chatid.'&text='.$text);
+    };
+
     echo "Timer was set";
     while (true) {
         $cursecs = time() + 10800;
-        $curdatetime = date("D h:i", $cursecs);
+        $currhours = intval(date("H", $cursecs));
+        // $currday = date("D", $cursecs);
+        $currday = intval(date("w", $cursecs));
 
-        file_put_contents('logs.txt', $curdatetime."\n", FILE_APPEND);
-        file_put_contents('logs.txt', $curdatetime."\n\n", FILE_APPEND);
-        sleep(10);
+        if ($currday == 5 && $currhours < 1) { // Friday
+            SendMessage(-1001359755141, "УРА! Пятница! Завтра выходные!!!");
+        }
+        if ($currday == 1 && $currhours < 1) { // Monday
+            SendMessage(-1001359755141, "Бля, ненавижу сраные понедельники. Сегодня как раз он.");
+        }
+        
+        sleep(1860);
     }
 
 ?>
