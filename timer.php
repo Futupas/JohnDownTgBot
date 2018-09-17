cfdd3b1909c7f0fcf125fc324d490bfa0d2baa5d
Olexandr Panov <futupas@gmail.com>
2018-09-17 23:56:23 +0300

    timer using while true

--- timer.php
+++ timer.php
@@ -1,12 +1,14 @@
 <?php
-    $MainTimer = new EvTimer(10, 1, function () {
-    // $MainTimer = new EvTimer(1860, 1, function () {
-        echo "2 seconds elapsed\n";
-        
+    // https://johndowntgbot.herokuapp.com/timer.php
+
+    echo "Timer was set";
+    while (true) {
         $cursecs = time() + 10800;
         $curdatetime = date("D h:i", $cursecs);
 
         file_put_contents('logs.txt', $curdatetime."\n", FILE_APPEND);
         file_put_contents('logs.txt', $curdatetime."\n\n", FILE_APPEND);
-    });
+        sleep(10);
+    }
+
 ?>
