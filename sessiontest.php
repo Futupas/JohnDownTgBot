3336387c89f08aee36c769e8ccfadcf70416ae7d
Olexandr Panov <futupas@gmail.com>
2018-10-05 20:23:19 +0300

    rem bug

--- sessiontest.php
+++ sessiontest.php
@@ -1,10 +1,10 @@
 <?php
     //
     session_start();
-    if(isset($_POST['testvar1'])) {
-        echo $_POST['testvar1'];
-        $_POST['testvar1'] = $_POST['testvar1'] + 1;
+    if(isset($_SESSION['testvar1'])) {
+        echo $_SESSION['testvar1'];
+        $_SESSION['testvar1'] = $_SESSION['testvar1'] + 1;
     } else {
-        $_POST['testvar1'] = 0;
+        $_SESSION['testvar1'] = 0;
     }
 ?>
