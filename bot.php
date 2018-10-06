190e01b9246a557d4f993680bd5ce5e631543f83
Olexandr Panov <futupas@gmail.com>
2018-10-06 22:36:36 +0300

    коммин

--- bot.php
+++ bot.php
@@ -64,7 +64,9 @@
                 $memmsg = $words[2];
                 $_SESSION['zapomni_msg_'.$memmsg] = $requestData->message->reply_to_message->message_id;
                 $_SESSION['zapomni_chatid_'.$memmsg] = $requestData->message->reply_to_message->chat->id;
+
                 file_put_contents('logs.txt', '1if'."\n", FILE_APPEND);
+                file_put_contents('logs.txt', $memmsg."\n", FILE_APPEND);
                 file_put_contents('logs.txt', $_SESSION['zapomni_chatid_'.$memmsg]."\n\n", FILE_APPEND);
             }
         }
@@ -73,8 +75,11 @@
             $memmsg = $words[2];
             $memmessid = $_SESSION['zapomni_msg_'.$memmsg];
             $oldchatid = $_SESSION['zapomni_chatid_'.$memmsg];
+
             file_put_contents('logs.txt', '2if'."\n", FILE_APPEND);
+            file_put_contents('logs.txt', $memmsg."\n", FILE_APPEND);
             file_put_contents('logs.txt', $_SESSION['zapomni_chatid_'.$memmsg]."\n\n", FILE_APPEND);
+            
             $response = file_get_contents(
                 'https://api.telegram.org/bot'.getenv('bot_token').'/forwardMessage?chat_id='.$msg_chatid.'&from_chat_id='.$oldchatid.'&message_id='.$memmessid
             );
