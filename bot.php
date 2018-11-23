38f61ce1217ca062e8be0ae875dbbcaffe51ac0e
Olexandr Panov <futupas@gmail.com>
2018-11-23 14:29:00 +0200

    commit

--- bot.php
+++ bot.php
@@ -94,7 +94,7 @@
             // ReplyToMessage($msg_chatid, $d_left->format("Y-m-d H:i:s"), $msg_id);
             ReplyToMessage($msg_chatid, $d_iee->format("Y-m-d H:i:s")." \n ".$d_now->format("Y-m-d H:i:s")." \n ".$d_left, $msg_id);
             // $d_iee->diff($d_now)->format("%d")
-            SendMessage($msg_chatid, $d_iee->diff($d_now)->format("%d")); 
+            SendMessage($msg_chatid, $d_iee->diff($d_now)->format("Y-m-d H:i:s")); 
             //
             exit(0);
         }
