c8d61df4593d9052f94c5b32e208efd2272aa8cb
Olexandr Panov <futupas@gmail.com>
2019-02-08 17:30:54 +0200

    wiki test

--- bot.php
+++ bot.php
@@ -189,7 +189,7 @@
             $link1 = $resultwiki[3][0];
 
             $message = "**".$title1."**\r\n".$content1."\r\n"."[Wikipedia[(".$link1.")";
-            $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?parse_mode=Markdown&chat_id='.$msg_chatid.'&text='.urlencode($message));
+            $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?parse_mode=Markdown&chat_id='.$msg_chatid.'&text='.$message);
             exit(0);
         }
         
