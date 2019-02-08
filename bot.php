1c7481d3b8b310f16564578d4d3aa27717816180
Olexandr Panov <futupas@gmail.com>
2019-02-08 17:21:40 +0200

    wiki test

--- bot.php
+++ bot.php
@@ -183,13 +183,13 @@
             $query = str_replace("вики", "", $msg);
             $getjson = file_get_contents('https://ru.wikipedia.org/w/api.php?action=opensearch&prop=info&format=json&inprop=url&search='.$query);
             // file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?chat_id='.$chatid.'&text='.$query);
-            $resultwiki = json_decode($getjson);
-            $title1 = $resultwiki[1][0];
-            $content1 = $resultwiki[2][0];
-            $link1 = $resultwiki[3][0];
+            // $resultwiki = json_decode($getjson);
+            // $title1 = $resultwiki[1][0];
+            // $content1 = $resultwiki[2][0];
+            // $link1 = $resultwiki[3][0];
 
-            $message = "**".$title1."**\r\n".$content1."\r\n"."[Wikipedia[(".$link1.")";
-            $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?parse_mode=Markdown&chat_id='.$msg_chatid.'&text='.$message);
+            // $message = "**".$title1."**\r\n".$content1."\r\n"."[Wikipedia[(".$link1.")";
+            $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?parse_mode=Markdown&chat_id='.$msg_chatid.'&text='.$getjson);
             exit(0);
         }
         
