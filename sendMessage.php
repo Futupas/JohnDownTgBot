32e2e6cb92ad53445e2277468a852b14ac349254
Olexandr Panov <futupas@gmail.com>
2020-01-10 14:34:05 +0200

    commit

--- sendMessage.php
+++ sendMessage.php
@@ -1,12 +1,12 @@
 <?php
 
-// echo
-// file_get_contents('https://api.telegram.org/bot'.$_GET['bot_token'].'/sendMessage?chat_id='.$_GET['chat_id'].'&parse_mode=Markdown&text='.$_GET['text']);
+echo
+file_get_contents('https://api.telegram.org/bot'.$_GET['bot_token'].'/sendMessage?chat_id='.$_GET['chat_id'].'&parse_mode=Markdown&text='.$_GET['text']);
 
 
 
-    $result = file_get_contents("https://api.telegram.org/bot860940041:AAE8lRsQUHMkpF1rlgUyX27PEiFXSd55Sh0/sendMessage?chat_id=649365656&text=hello world");
+    // $result = file_get_contents("https://api.telegram.org/bot860940041:AAE8lRsQUHMkpF1rlgUyX27PEiFXSd55Sh0/sendMessage?chat_id=649365656&text=hello world");
 
-    echo $result;
+    // echo $result;
 
 ?>
